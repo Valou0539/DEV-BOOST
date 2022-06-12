01ebb4d96b4b4a883751b910769514a01cfcf205
Valou0539 <87665858+Valou0539@users.noreply.github.com>
2022-06-12 17:15:38 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 const body = document.querySelector('body')
-const moonButton = document.querySelector('.moon')
-const sunButton = document.querySelector('.sun')
+const moonButtons = document.querySelectorAll('.moon')
+const sunButtons = document.querySelectorAll('.sun')
 
-moonButton.addEventListener('click', e => {
+moonButtons.forEach(moonButton => moonButton.addEventListener('click', e => {
     if ((body.classList[0] === 'light_theme') === true) {
         body.classList.remove('light_theme')
     }
     if ((body.classList[0] === 'dark_theme') === false) {
         body.classList.add('dark_theme')
     }
-})
+}))
 
-sunButton.addEventListener('click', e => {
+sunButtons.forEach(sunButton => sunButton.addEventListener('click', e => {
     if ((body.classList[0] === 'dark_theme') === true) {
         body.classList.remove('dark_theme')
     }
     if ((body.classList[0] === 'light_theme') === false) {
         body.classList.add('light_theme')
     }
-})
+}))
